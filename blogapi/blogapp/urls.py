e4ea513a574db109c7e8b36bfd3a4c0cc74b5bd2
samghadri <sam.ghadri@gmail.com>
2017-11-29 20:15:19 +0000

    Postgressetup&postdetailAPiview

--- blogapi/blogapp/urls.py
+++ blogapi/blogapp/urls.py
@@ -1,8 +1,10 @@
+#MyBlog App Url
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
 urlpatterns =[
     url(r'^$',views.PostApi.as_view(),name='list'),
+    url(r'^(?P<slug>[\w-]+)/$',views.PostDetailApi.as_view(),name='detail'),
 
 ]
--- blogapi/blogapp/views.py
+++ blogapi/blogapp/views.py
@@ -1,7 +1,11 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Post
 from .serializers import PostSerializer
 
 class PostApi(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailApi(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
